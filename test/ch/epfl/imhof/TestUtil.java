d3d9c77094d9ad3826234f4a51ffba82ce4cbfbe
Thierry Treyer <zehkae@gmail.com>
2015-03-03 11:00:10 +0100

    Ajout de TestUtil pour reprendre les méthodes couramment utilisées

---
+++ test/ch/epfl/imhof/TestUtil.java
@@ -0,0 +1,136 @@
+package ch.epfl.imhof;
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.LinkedList;
+
+import ch.epfl.imhof.*;
+import ch.epfl.imhof.osm.*;
+import static ch.epfl.imhof.osm.OSMRelation.Member.Type.*;
+
+public abstract class TestUtil {
+	public static final double DOUBLE_DELTA = 10e-6;
+	public static final String DEFAULT_ROLE = "testing";
+
+	public static int ID = 1;
+
+	/** Méthode de créations */
+	public static Map<String, String> newMap () {
+		Map<String, String> m = new HashMap<String, String>();
+
+		m.put("foo", "bar");
+		m.put("int", "1337");
+		m.put("float", "3.14159");
+
+		return m;
+	}
+
+	public static int newId () {
+		return ID++;
+	}
+
+	public static Attributes newAttributes (Map<String, String> attrs) {
+		Attributes.Builder ab = new Attributes.Builder();
+
+		for (String key : attrs.keySet())
+			ab.put(key, attrs.get(key));
+
+		return ab.build();
+	}
+
+	public static Attributes newAttributes () {
+		return newAttributes(newMap());
+	}
+
+	public static PointGeo newPointGeo (double longitude, double latitude) {
+		return new PointGeo(longitude, latitude);
+	}
+
+	public static PointGeo newPointGeo () {
+		double longitude = (Math.random() * Math.PI * 2) - Math.PI;
+		double latitude = (Math.random() * Math.PI) - (Math.PI / 2);
+
+		return newPointGeo(longitude, latitude);
+	}
+
+	public static OSMNode newOSMNode () {
+		return new OSMNode(newId(), newPointGeo(), newAttributes());
+	}
+
+	public static List<OSMNode> newOSMNodeList () {
+		LinkedList<OSMNode> nodeList = new LinkedList<OSMNode>();
+
+		nodeList.add(newOSMNode());
+		nodeList.add(newOSMNode());
+		nodeList.add(newOSMNode());
+
+		return nodeList;
+	}
+
+	public static OSMWay newOSMWay () {
+		return new OSMWay(newId(), newOSMNodeList(), newAttributes());
+	}
+
+	public static List<OSMRelation.Member> newSimpleOSMMemberList () {
+		List<OSMRelation.Member> members = new LinkedList<OSMRelation.Member>();
+
+		members.add(new OSMRelation.Member(NODE, DEFAULT_ROLE, newOSMNode()));
+		members.add(new OSMRelation.Member(WAY, DEFAULT_ROLE, newOSMWay()));
+
+		return members;
+	}
+
+	public static OSMRelation newSimpleOSMRelation () {
+		return new OSMRelation(newId(), newSimpleOSMMemberList(), newAttributes());
+	}
+
+	public static List<OSMRelation.Member> newMemberList () {
+		List<OSMRelation.Member> members = new LinkedList<OSMRelation.Member>();
+
+		members.add(new OSMRelation.Member(NODE, DEFAULT_ROLE, newOSMNode()));
+		members.add(new OSMRelation.Member(WAY, DEFAULT_ROLE, newOSMWay()));
+		members.add(new OSMRelation.Member(RELATION, DEFAULT_ROLE, newSimpleOSMRelation()));
+
+		return members;
+	}
+
+	/** Méthodes d'assertion */
+	public static void assertPositionEquals (PointGeo expected, PointGeo point) {
+		assertEquals(expected.longitude(), point.longitude(), DOUBLE_DELTA);
+		assertEquals(expected.latitude(), point.latitude(), DOUBLE_DELTA);
+	}
+
+	public static void assertAttributesEquals (Attributes expected, Attributes attributes, Set<String> keys) {
+		assertEquals(expected.size(), attributes.size());
+
+		for (String key : keys)
+			assertEquals(expected.get(key), attributes.get(key));
+	}
+
+	public static void assertAttributesEquals (Attributes expected, Attributes attributes, Map<String, String> attrs) {
+		assertAttributesEquals(expected, attributes, attrs.keySet());
+	}
+
+	public static void assertAttributesEquals (Attributes expected, Attributes attributes) {
+		assertAttributesEquals(expected, attributes, newMap());
+	}
+
+	public static void assertNodeEquals (OSMNode expected, OSMNode node, Set<String> attrKeys) {
+		assertEquals(expected.id(), node.id());
+		assertPositionEquals(expected.position(), node.position());
+		assertAttributesEquals(expected.attributes(), node.attributes(), attrKeys);
+	}
+
+	public static void assertNodeEquals (OSMNode expected, OSMNode node, Map<String, String> attrs) {
+		assertNodeEquals(expected, node, attrs.keySet());
+	}
+
+	public static void assertNodeEquals (OSMNode expected, OSMNode node) {
+		assertNodeEquals(expected, node, newMap());
+	}
+}
--- test/ch/epfl/imhof/osm/OSMRelationTest.java
+++ test/ch/epfl/imhof/osm/OSMRelationTest.java
@@ -3,132 +3,29 @@ package ch.epfl.imhof.osm;
 import org.junit.*;
 import static org.junit.Assert.*;
 
-import java.util.Set;
-import java.util.HashSet;
-import java.util.List;
-import java.util.LinkedList;
+import static ch.epfl.imhof.TestUtil.*;
 
-import ch.epfl.imhof.PointGeo;
-import ch.epfl.imhof.Attributes;
+import java.util.List;
+
+import static ch.epfl.imhof.osm.OSMRelation.Member.Type.*;
 
 public class OSMRelationTest {
-	private static int ID = 1;
-
-	private static final double DOUBLE_DELTA = 0.000001;
-	private static final String ATTR_STRING_KEY = "foo";
-	private static final String ATTR_STRING_VALUE = "bar";
-	private static final String ATTR_INT_KEY = "int";
-	private static final String ATTR_INT_VALUE = "1337";
-	private static final String UNKNOWN_KEY = "randomkey";
-
-	private static final PointGeo DEFAULT_POSITION = new PointGeo(1.2, 0.7);
-	private static final OSMNode MEMBER_NODE = newNode();
-	private static final OSMRelation.Member.Type MEMBER_NODE_TYPE = OSMRelation.Member.Type.NODE;
-	private static final OSMRelation.Member.Type MEMBER_WAY_TYPE = OSMRelation.Member.Type.WAY;
-	private static final OSMRelation.Member.Type MEMBER_RELATION_TYPE = OSMRelation.Member.Type.RELATION;
-	private static final String MEMBER_ROLE = "testing";
-
-	private static Attributes newAttributes () {
-		Attributes.Builder ab = new Attributes.Builder();
-
-		ab.put(ATTR_STRING_KEY, ATTR_STRING_VALUE);
-		ab.put(ATTR_INT_KEY, ATTR_INT_VALUE);
-
-		return ab.build();
-	}
-
-	private static int newId () { return ID++; }
-
-	private static OSMNode newNode () {
-		return new OSMNode(newId(), DEFAULT_POSITION, newAttributes());
-	}
-
-	private static List<OSMNode> newNodeList () {
-		LinkedList<OSMNode> nodeList = new LinkedList<OSMNode>();
-
-		nodeList.add(newNode());
-		nodeList.add(newNode());
-		nodeList.add(newNode());
-
-		return nodeList;
-	}
-
-	private static OSMWay newWay () {
-		return new OSMWay(newId(), newNodeList(), newAttributes());
-	}
-
-	private static List<OSMRelation.Member> newSimpleMemberList () {
-		List<OSMRelation.Member> members = new LinkedList<OSMRelation.Member>();
-
-		members.add(new OSMRelation.Member(MEMBER_NODE_TYPE, MEMBER_ROLE, newNode()));
-		members.add(new OSMRelation.Member(MEMBER_WAY_TYPE, MEMBER_ROLE, newWay()));
-
-		return members;
-	}
-
-	private static OSMRelation newSimpleRelation () {
-		return new OSMRelation(newId(), newSimpleMemberList(), newAttributes());
-	}
-
-	private static List<OSMRelation.Member> newMemberList () {
-		List<OSMRelation.Member> members = new LinkedList<OSMRelation.Member>();
-
-		members.add(new OSMRelation.Member(MEMBER_NODE_TYPE, MEMBER_ROLE, newNode()));
-		members.add(new OSMRelation.Member(MEMBER_WAY_TYPE, MEMBER_ROLE, newWay()));
-		members.add(new OSMRelation.Member(MEMBER_RELATION_TYPE, MEMBER_ROLE, newSimpleRelation()));
-
-		return members;
-	}
-
-	private static OSMRelation newRelation () {
-		return new OSMRelation(newId(), newMemberList(), newAttributes());
-	}
-
-	private void assertAttributesEquals (Attributes expected, Attributes attributes, Set<String> keys) {
-		assertEquals(expected.size(), attributes.size());
-
-		for (String key : keys)
-			assertEquals(expected.get(key), attributes.get(key));
-	}
-
-	private void assertPositionEquals (PointGeo expected, PointGeo point) {
-		assertEquals(expected.longitude(), point.longitude(), DOUBLE_DELTA);
-		assertEquals(expected.latitude(), point.latitude(), DOUBLE_DELTA);
-	}
-
-	private void assertNodeEquals (OSMNode expected, OSMNode node) {
-		Set<String> keys = new HashSet<String>();
-		keys.add(ATTR_STRING_KEY);
-		keys.add(ATTR_INT_KEY);
-
-		assertEquals(expected.id(), node.id());
-
-		assertEquals(expected.attributes(), node.attributes());
-		assertAttributesEquals(expected.attributes(), node.attributes(), keys);
-
-		assertEquals(expected.position(), node.position());
-		assertPositionEquals(expected.position(), node.position());
-	}
-
 	private void assertMemberEquals (OSMRelation.Member expected, OSMRelation.Member member) {
 		assertEquals(expected.type(), member.type());
 		assertEquals(expected.role(), member.role());
 		assertEquals(expected.member(), member.member());
 	}
 
-    @Before
-    public void setUp() {
-    }
-
     @Test
     public void testOSMRelationMember () {
-		OSMRelation.Member member = new OSMRelation.Member(MEMBER_NODE_TYPE, MEMBER_ROLE, MEMBER_NODE);
+		OSMNode node = newOSMNode();
+		OSMRelation.Member member = new OSMRelation.Member(NODE, DEFAULT_ROLE, node);
 
 		assertNotNull(member);
-		assertEquals(MEMBER_NODE_TYPE, member.type());
-		assertEquals(MEMBER_ROLE, member.role());
-		assertEquals(MEMBER_NODE, member.member());
-		assertNodeEquals(MEMBER_NODE, (OSMNode)member.member());
+		assertEquals(NODE, member.type());
+		assertEquals(DEFAULT_ROLE, member.role());
+		assertTrue(node == member.member());
+		assertNodeEquals(node, (OSMNode)member.member());
     }
 
 	@Test
@@ -169,7 +66,6 @@ public class OSMRelationTest {
 		OSMRelation relation = new OSMRelation(newId(), members, newAttributes());
 
 		List<OSMRelation.Member> relationMembers = relation.members();
-
 
 		assertTrue(members != relation.members());
 
