2c09b346f3eaacd11773a2b62e2c5e929f31ac21
droduit <dominique.roduit@epfl.ch>
2015-03-09 13:00:49 +0100

    classe OSMMap terminée + doc Ok

--- src/ch/epfl/imhof/osm/OSMMap.java
+++ src/ch/epfl/imhof/osm/OSMMap.java
@@ -2,7 +2,9 @@ package ch.epfl.imhof.osm;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Représente une carte OpenStreetMap, c'est-à-dire un ensemble de chemins et de relations
@@ -23,60 +25,65 @@ public final class OSMMap {
      *
      */
     public static final class Builder {
+        private Map<Long, OSMNode> nodes = new HashMap<>();
+        private Map<Long, OSMWay> ways = new HashMap<>();
+        private Map<Long, OSMRelation> relations = new HashMap<>();
+        
+        
         /**
          * Ajoute le nœud donné au bâtisseur
-         * @param newNode
+         * @param newNode Nouveau noeud à ajouter au bâtisseur
          */
         public void addNode(OSMNode newNode) {
-            
+            nodes.put(newNode.id(),  newNode);
         }
         /**
          * Retourne le nœud dont l'identifiant unique est égal à celui donné,
          * ou null si ce nœud n'a pas été ajouté précédemment au bâtisseur
-         * @param id
-         * @return
+         * @param id Identifiant unique du noeud
+         * @return le noeud correspondant à l'identifiant passé
          */
         public OSMNode nodeForId(long id){
-            
+            return nodes.get(id);
         }
         /**
          * Ajoute le chemin donné à la carte en cours de construction
-         * @param newWay
+         * @param newWay Nouveau chemin à ajouter à la Map
          */
         public void addWay(OSMWay newWay) {
-            
+            ways.put(newWay.id(), newWay);
         }
         /**
          * Retourne le chemin dont l'identifiant unique est égal à celui donné,
          * ou null si ce chemin n'a pas été ajouté précédemment au bâtisseur
-         * @param id
-         * @return
+         * @param id Identifiant unique du chemin que l'on veut recupérer
+         * @return Chemin correspondant à l'identifiant passé en paramètre
          */
         public OSMWay wayForId(long id){
-            
+            return ways.get(id);
         }
         /**
          * Ajoute la relation donnée à la carte en cours de construction
-         * @param newRelation
+         * @param newRelation Nouvelle relation à ajouter à la carte
          */
         public void addRelation(OSMRelation newRelation) {
-            
+            relations.put(newRelation.id(), newRelation);
         }
         /**
          * Retourne la relation dont l'identifiant unique est égal à celui donné,
          * ou null si cette relation n'a pas été ajoutée précédemment au bâtisseur
-         * @param id
-         * @return
+         * @param id Identifiant unique de la relation à récupérer
+         * @return La relation correspondant à l'identifiant passé en paramètre
          */
         public OSMRelation relationForId(long id) {
-            
+            return relations.get(id);
         }
         /**
          * Construit une carte OSM avec les chemins et les relations ajoutés jusqu'à présent.
-         * @return
+         * @return Carte OSM construite sur la base des chemins et relations ajoutés jusqu'à présent
          */
         public OSMMap build() {
-            
+            return new OSMMap(ways.values(), relations.values());
         }
         
 
