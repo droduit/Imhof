1abf0c9dc6054aede16e5576a5b402fa1494b332
droduit <dominique.roduit@epfl.ch>
2015-05-01 17:43:05 +0200

    Etape 10

---
+++ src/ch/epfl/imhof/dem/Earth.java
@@ -0,0 +1,14 @@
+package ch.epfl.imhof.dem;
+
+/**
+ * Contient des constantes liées à la Terre
+ * @author Thierry Treyer (235116)
+ * @author Dominique Roduit (234868)
+ *
+ */
+public interface Earth {
+    /**
+     * Rayon de la terre en mètres
+     */
+    public final static int RADIUS = 6_378_137;
+}
