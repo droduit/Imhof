3520c3f0aa12fab016fcb392182de97e8122751c
droduit <dominique.roduit@epfl.ch>
2015-05-18 11:42:22 +0200

    Doc relief shader, allons manger

--- src/ch/epfl/imhof/dem/ReliefShader.java
+++ src/ch/epfl/imhof/dem/ReliefShader.java
@@ -11,17 +11,34 @@ import ch.epfl.imhof.Vector3;
 import ch.epfl.imhof.geometry.Point;
 import ch.epfl.imhof.projection.Projection;
 
+/**
+ * Permet de dessiner un relief ombré coloré
+ * @author Thierry Treyer (235116)
+ * @author Dominique Roduit (234868)
+ *
+ */
 public final class ReliefShader {
     private final Projection projection;
     private final DigitalElevationModel data;
     private final Vector3 lightDirection;
 
+    /**
+     * Construit une instance d'un relief ombré
+     * @param projection Projection à utiliser
+     * @param data Modèle numérique du terrain
+     * @param lightDirection Vecteur pointant dans la direction de la source lumineuse
+     */
     public ReliefShader (Projection projection, DigitalElevationModel data, Vector3 lightDirection) {
         this.projection = projection;
         this.data = data;
         this.lightDirection = lightDirection.normalized();
     }
 
+    /**
+     * Permet de calculer le noyau du flou gaussien étant donné son rayon
+     * @param radius Rayon du flou gaussien
+     * @return Données du noyau
+     */
     private float[] buildKernelData (float radius) {
         int o = (int)Math.ceil(radius);
         int n = 2 * o + 1;
@@ -44,6 +61,15 @@ public final class ReliefShader {
         return data;
     }
 
+    /**
+     * Permet de dessiner un relief ombré brut (sans floutage)
+     * @param bottomLeft  Coin bas-gauche du relief à dessiner
+     * @param topRight Coin haut-droit du relief à dessiner
+     * @param width Largeur de l'image
+     * @param height Hauteur de l'image
+     * @param blurOffset Décalage du flou comme on ne floute pas les bords
+     * @return Image du relief ombré brut
+     */
     private BufferedImage drawRawRelief (Point bottomLeft, Point topRight, int width, int height, int blurOffset) {
         int rawWidth = width + 2 * blurOffset;
         int rawHeight = height + 2 * blurOffset;
@@ -70,6 +96,15 @@ public final class ReliefShader {
         return relief;
     }
 
+    /**
+     * 
+     * @param bottomLeft Coin bas-gauche du relief à dessiner en coordonnées du plan
+     * @param topRight Coin haut-droit du relief à dessiner en coordonnées du plan
+     * @param width Largeur de l'image à dessiner (px)
+     * @param height Hauteur de l'image à dessiner (px)
+     * @param gaussRadius Rayon de floutage
+     * @return Image du relief flouté
+     */
     public BufferedImage shadedRelief (Point bottomLeft, Point topRight, int width, int height, float gaussRadius) {
         int pixelRadius = (int)Math.ceil(gaussRadius);
 
